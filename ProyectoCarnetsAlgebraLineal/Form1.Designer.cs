b95846bf2f2f94fe565b2cd5255912aa358e371c
Julián Vásquez <julianvg@umes.edu.gt>
2024-05-14 20:44:21 -0600

    Actualizacion Randomizar

--- ProyectoCarnetsAlgebraLineal/Form1.Designer.cs
+++ ProyectoCarnetsAlgebraLineal/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.btnGenerar = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
@@ -36,24 +37,32 @@
             this.txtCarnetNuevo = new System.Windows.Forms.TextBox();
             this.btnGenerarQr = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(126, 9);
+            this.label1.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(400, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(201, 22);
+            this.label1.Size = new System.Drawing.Size(282, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Generador De Carnets";
             // 
             // btnGenerar
             // 
-            this.btnGenerar.Location = new System.Drawing.Point(24, 94);
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(297, 102);
             this.btnGenerar.Name = "btnGenerar";
-            this.btnGenerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerar.Size = new System.Drawing.Size(64, 34);
             this.btnGenerar.TabIndex = 1;
             this.btnGenerar.Text = "Generar";
             this.btnGenerar.UseVisualStyleBackColor = true;
@@ -62,15 +71,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 52);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(294, 59);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.Size = new System.Drawing.Size(188, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Ingrese el Carnet Actual:";
             // 
             // txtCarnetActual
             // 
-            this.txtCarnetActual.Location = new System.Drawing.Point(24, 68);
+            this.txtCarnetActual.Location = new System.Drawing.Point(297, 79);
             this.txtCarnetActual.Name = "txtCarnetActual";
             this.txtCarnetActual.Size = new System.Drawing.Size(120, 20);
             this.txtCarnetActual.TabIndex = 3;
@@ -78,24 +88,26 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 124);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(294, 139);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.Size = new System.Drawing.Size(138, 17);
             this.label4.TabIndex = 5;
             this.label4.Text = "Carnet Generado:";
             // 
             // txtCarnetNuevo
             // 
-            this.txtCarnetNuevo.Location = new System.Drawing.Point(27, 140);
+            this.txtCarnetNuevo.Location = new System.Drawing.Point(297, 158);
             this.txtCarnetNuevo.Name = "txtCarnetNuevo";
             this.txtCarnetNuevo.Size = new System.Drawing.Size(120, 20);
             this.txtCarnetNuevo.TabIndex = 6;
             // 
             // btnGenerarQr
             // 
-            this.btnGenerarQr.Location = new System.Drawing.Point(27, 193);
+            this.btnGenerarQr.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarQr.Location = new System.Drawing.Point(297, 183);
             this.btnGenerarQr.Name = "btnGenerarQr";
-            this.btnGenerarQr.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerarQr.Size = new System.Drawing.Size(64, 41);
             this.btnGenerarQr.TabIndex = 7;
             this.btnGenerarQr.Text = "Generar QR";
             this.btnGenerarQr.UseVisualStyleBackColor = true;
@@ -103,17 +115,62 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(27, 222);
+            this.pictureBox1.Location = new System.Drawing.Point(280, 248);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(497, 353);
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGreen;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(248, 623);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(0, -8);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(252, 136);
+            this.panel2.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(51, 11);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(135, 119);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(422, 159);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 19);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Randomizar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(948, 645);
+            this.ClientSize = new System.Drawing.Size(814, 623);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnGenerarQr);
             this.Controls.Add(this.txtCarnetNuevo);
@@ -122,11 +179,14 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnGenerar);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,6 +202,10 @@
         private System.Windows.Forms.TextBox txtCarnetNuevo;
         private System.Windows.Forms.Button btnGenerarQr;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ProyectoCarnetsAlgebraLineal/Form1.cs
+++ ProyectoCarnetsAlgebraLineal/Form1.cs
@@ -14,6 +14,7 @@ namespace ProyectoCarnetsAlgebraLineal
 {
     public partial class Form1 : Form
     {
+        string nuevoCodigo;
         public Form1()
         {
             InitializeComponent();
@@ -22,12 +23,23 @@ namespace ProyectoCarnetsAlgebraLineal
         private void btnGenerar_Click(object sender, EventArgs e)
         {
             string carnet = txtCarnetActual.Text;
+
+            // Validación del formato y rango de años del carnet
             if (carnet.Length != 9)
             {
                 MessageBox.Show("El formato del carnet es incorrecto. Debe tener 9 dígitos.");
                 return;
             }
 
+            string yearStr = carnet.Substring(0, 4);
+            int year;
+            if (!int.TryParse(yearStr, out year) || year < 2016 || year > 2024)
+            {
+                MessageBox.Show("El año en el carnet debe estar entre 2016 y 2024.");
+                return;
+            }
+
+            // Continuar con la generación del código y la lógica existente
             int suma = 0;
             for (int i = 0; i < carnet.Length; i++)
             {
@@ -36,16 +48,30 @@ namespace ProyectoCarnetsAlgebraLineal
             int digitoVerificacion = suma % 11;
             string digitoVerificacionStr = digitoVerificacion == 10 ? "X" : digitoVerificacion.ToString();
 
-            string nuevoCodigo = carnet + digitoVerificacionStr;
+             nuevoCodigo = carnet + digitoVerificacionStr;
+            string temporal = carnet + "*";
 
             // Mostramos el nuevo código en el cuadro de texto correspondiente
-            txtCarnetNuevo.Text = nuevoCodigo;
+            txtCarnetNuevo.Text = temporal;
 
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int digitoVerificacionAleatorio = rnd.Next(10); // Genera un número aleatorio entre 0 y 9
+            string nuevoDigitoVerificacionStr = digitoVerificacionAleatorio == 10 ? "X" : digitoVerificacionAleatorio.ToString();
 
+            // Actualiza el dígito de verificación en el nuevo código del carné
+            nuevoCodigo = nuevoCodigo.Substring(0, nuevoCodigo.Length - 1) + nuevoDigitoVerificacionStr;
+
+            // Muestra el nuevo código con el dígito de verificación aleatorio en el cuadro de texto correspondiente
+            txtCarnetNuevo.Text = nuevoCodigo;
+
+
+        }
         private void btnGenerarQr_Click(object sender, EventArgs e)
         {
-            string carnet = txtCarnetNuevo.Text;
+            string carnet = nuevoCodigo;
 
             var qrWriter = new ZXing.BarcodeWriter
             {
@@ -61,7 +87,8 @@ namespace ProyectoCarnetsAlgebraLineal
             pictureBox1.Image = bitmap;
         }
 
-        }
+        
+    }
     
 
 }
